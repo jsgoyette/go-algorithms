fcce043e7f58cf45276ea1875638acb6f7854f04
Jeremiah Goyette <jsgoyette@protonmail.com>
2019-02-10 08:53:28 +0000

    Pull heap sort logic out of main func

--- sorting/heap_sort.go
+++ sorting/heap_sort.go
@@ -6,54 +6,52 @@ package main
 
 import "fmt"
 
-import "github.com/0xAX/go-algorithms"
+import "github.com/jsgoyette/go-algorithms"
 
-func sift(arr []int, i int, arrLen int) []int {
-    done := false
+func siftDown(arr []int, i int, arrLen int) []int {
+	done := false
+	maxChild := 0
 
-    tmp       := 0
-    maxChild  := 0
-    
-    for (i * 2 + 1 < arrLen) && (!done) {
-        if i * 2 + 1 == arrLen - 1 {
-            maxChild = i * 2 + 1
-        } else if (arr[i * 2 + 1] > arr[i * 2 + 2]) {
-            maxChild = i * 2 + 1
-        } else {
-            maxChild = i * 2 + 2
-        }
-    
-        if arr[i] < arr[maxChild] {
-            tmp = arr[i]
-            arr[i] = arr[maxChild]
-            arr[maxChild] = tmp
-            i = maxChild
-        } else {
-            done = true
-        }
-    }
+	for i*2+1 < arrLen && !done {
+		if i*2+1 == arrLen-1 {
+			maxChild = i*2 + 1
+		} else if arr[i*2+1] > arr[i*2+2] {
+			maxChild = i*2 + 1
+		} else {
+			maxChild = i*2 + 2
+		}
 
-    return arr
+		if arr[i] < arr[maxChild] {
+			arr[i], arr[maxChild] = arr[maxChild], arr[i]
+			i = maxChild
+		} else {
+			done = true
+		}
+	}
+
+	return arr
+}
+
+func heapify(arr []int) {
+	for i := len(arr)/2 - 1; i >= 0; i-- {
+		arr = siftDown(arr, i, len(arr))
+	}
+}
+
+func heapSort(arr []int) {
+	for i := len(arr) - 1; i >= 1; i-- {
+		arr[i], arr[0] = arr[0], arr[i]
+		arr = siftDown(arr, 0, i)
+	}
 }
 
 func main() {
-    arr := utils.RandArray(10)
-    fmt.Println("Initial array is:", arr)
-    fmt.Println("")
+	arr := utils.RandArray(12)
+	fmt.Println("Initial array is:", arr)
 
-    i := 0
-    tmp := 0
+	heapify(arr)
+	fmt.Println("Heapify array is:", arr)
 
-    for i = len(arr) / 2 - 1; i >= 0; i-- {
-        arr = sift(arr, i, len(arr))
-    }
-    
-    for i = len(arr) - 1; i >= 1; i-- {
-        tmp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = tmp;
-        arr = sift(arr, 0, i);
-    }
-
-    fmt.Println("Sorted array is: ", arr)
+	heapSort(arr)
+	fmt.Println("Sorted array is: ", arr)
 }
